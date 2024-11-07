36f8b153ebf40f9c853726d3c9364ea1628ca61c
AdrianC2095 <adriancuenca095@gmail.com>
2024-11-07 10:23:31 -0500

    Completo

--- EjercicioNumeros/src/ejercicionumeros.java
+++ EjercicioNumeros/src/ejercicionumeros.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class ejercicionumeros {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        double num1, num2, num3, mayor;
+        double num1, num2, num3;
         System.out.print("Ingrese el primer número: ");
         num1 = teclado.nextDouble();
         System.out.print("Ingrese el segundo número: ");
@@ -10,16 +10,10 @@ public class ejercicionumeros {
         System.out.print("Ingrese el tercer número: ");
         num3 = teclado.nextDouble();        
         if (num1 > num2 && num1 > num3)
-                {
-                    mayor = num1;
-                    System.out.println("El número mayor es: " + mayor);
-                }
+                System.out.println("El número mayor es: " + num1);
                 else if (num2 > num1 && num2 > num3)
-                        {
-                            mayor = num2;
-                            System.out.println("El número mayor es: " + mayor);
-                        }
-                        else System.out.println("El número mayor es: " +  num3);
+                    System.out.println("El número mayor es: " + num2);
+                    else System.out.println("El número mayor es: " +  num3);
     }
     
 }
